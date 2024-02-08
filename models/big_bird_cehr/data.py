91173212d7c2be7286a5621a17ea0debff66f7ab
afallah <afallah@v3.cluster.local>
2024-02-07 21:25:10 -0500

    Updated XGBoost for new data

---
+++ models/big_bird_cehr/data.py
@@ -0,0 +1,157 @@
+import random
+from typing import Sequence, Union
+
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+from models.cehr_bert.tokenizer import ConceptTokenizer
+
+
+class PretrainDataset(Dataset):
+    """Dataset for pretraining the model."""
+
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        tokenizer: ConceptTokenizer,
+        max_len: int = 2048,
+        mask_prob: float = 0.15,
+    ):
+        self.data = data
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.mask_prob = mask_prob
+
+    def __len__(self):
+        """Return the length of the dataset."""
+        return len(self.data)
+
+    def tokenize_data(self, sequence: Union[str, Sequence[str]]) -> np.ndarray:
+        """Tokenize the sequence."""
+        tokenized = self.tokenizer.encode(sequence)
+        tokenized = np.array(tokenized).flatten()
+        return tokenized
+
+    def get_attention_mask(self, sequence: np.ndarray) -> np.ndarray:
+        """Get the attention mask for the sequence."""
+        attention_mask = [
+            float(token != self.tokenizer.get_pad_token_id()) for token in sequence
+        ]
+        return attention_mask
+
+    def mask_tokens(self, sequence: np.ndarray) -> tuple:
+        """Mask the tokens in the sequence."""
+        masked_sequence = []
+        labels = []
+        for token in sequence:
+            if token in self.tokenizer.get_special_token_ids():
+                masked_sequence.append(token)
+                labels.append(-100)
+                continue
+            prob = random.random()
+            if prob < self.mask_prob:
+                dice = random.random()
+                if dice < 0.8:
+                    masked_sequence.append(self.tokenizer.get_mask_token_id())
+                elif dice < 0.9:
+                    random_token = random.randint(
+                        self.tokenizer.get_first_token_index(),
+                        self.tokenizer.get_last_token_index(),
+                    )
+                    masked_sequence.append(random_token)
+                else:
+                    masked_sequence.append(token)
+                labels.append(token)
+            else:
+                masked_sequence.append(token)
+                labels.append(-100)
+        return masked_sequence, labels
+
+    def __getitem__(self, idx: int) -> dict:
+        data = self.data.iloc[idx]
+        concept_tokens = self.tokenize_data(data["event_tokens"])
+        age_tokens = data["age_tokens"]
+        time_tokens = data["time_tokens"]
+        visit_tokens = data["visit_tokens"]
+        position_tokens = data["position_tokens"]
+
+        attention_mask = self.get_attention_mask(concept_tokens)
+        masked_tokens, labels = self.mask_tokens(concept_tokens)
+
+        masked_tokens = torch.tensor(masked_tokens)
+        age_tokens = torch.tensor(age_tokens)
+        time_tokens = torch.tensor(time_tokens)
+        visit_tokens = torch.tensor(visit_tokens)
+        position_tokens = torch.tensor(position_tokens)
+        labels = torch.tensor(labels)
+        attention_mask = torch.tensor(attention_mask)
+
+        return {
+            "concept_ids": masked_tokens,
+            "ages": age_tokens,
+            "time_stamps": time_tokens,
+            "visit_orders": position_tokens,
+            "visit_segments": visit_tokens,
+            "labels": labels,
+            "attention_mask": attention_mask,
+        }
+
+
+class FinetuneDataset(Dataset):
+    """Dataset for finetuning the model."""
+
+    def __init__(
+        self,
+        data: pd.DataFrame,
+        tokenizer: ConceptTokenizer,
+        max_len: int = 2048,
+    ):
+        self.data = data
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def tokenize_data(self, sequence):
+        """Tokenize the sequence."""
+        tokenized = self.tokenizer.encode(sequence)
+        tokenized = np.array(tokenized).flatten()
+        return tokenized
+
+    def get_attention_mask(self, sequence):
+        """Get the attention mask for the sequence."""
+        attention_mask = [
+            float(token != self.tokenizer.get_pad_token_id()) for token in sequence
+        ]
+        return attention_mask
+
+    def __getitem__(self, idx):
+        data = self.data.iloc[idx]
+        concept_tokens = self.tokenize_data(data["event_tokens"])
+        age_tokens = data["age_tokens"]
+        time_tokens = data["time_tokens"]
+        visit_tokens = data["visit_tokens"]
+        position_tokens = data["position_tokens"]
+        labels = data["label"]
+        attention_mask = self.get_attention_mask(concept_tokens)
+
+        concept_tokens = torch.tensor(concept_tokens)
+        age_tokens = torch.tensor(age_tokens)
+        time_tokens = torch.tensor(time_tokens)
+        visit_tokens = torch.tensor(visit_tokens)
+        position_tokens = torch.tensor(position_tokens)
+        labels = torch.tensor(labels)
+        attention_mask = torch.tensor(attention_mask)
+
+        return {
+            "concept_ids": concept_tokens,
+            "ages": age_tokens,
+            "time_stamps": time_tokens,
+            "visit_orders": position_tokens,
+            "visit_segments": visit_tokens,
+            "labels": labels,
+            "attention_mask": attention_mask,
+        }
---
+++ models/big_bird_cehr/finetune.py
@@ -0,0 +1,206 @@
+import argparse
+import os
+from os.path import join
+
+import numpy as np
+import pandas as pd
+import pytorch_lightning as pl
+import torch
+from lightning.pytorch.loggers import WandbLogger
+from pytorch_lightning.callbacks import (
+    EarlyStopping,
+    LearningRateMonitor,
+    ModelCheckpoint,
+)
+from pytorch_lightning.strategies.ddp import DDPStrategy
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
+
+from models.cehr_bert.data import FinetuneDataset
+from models.cehr_bert.model import BertFinetune, BertPretrain
+from models.cehr_bert.tokenizer import ConceptTokenizer
+
+
+def main(args):
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    pl.seed_everything(args.seed)
+
+    os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+    torch.cuda.empty_cache()
+    torch.set_float32_matmul_precision("medium")
+
+    fine_data = pd.read_parquet(join(args.data_dir, "fine_tune.parquet"))
+    fine_train, fine_valtest = train_test_split(
+        fine_data,
+        train_size=args.train_size,
+        random_state=args.seed,
+        stratify=fine_data["label"],
+    )
+    fine_val, fine_test = train_test_split(
+        fine_valtest,
+        test_size=args.test_size,
+        random_state=args.seed,
+        stratify=fine_valtest["label"],
+    )
+
+    tokenizer = ConceptTokenizer()
+    tokenizer.fit_on_vocab()
+
+    train_dataset = FinetuneDataset(
+        data=fine_train,
+        tokenizer=tokenizer,
+        max_len=args.max_len,
+    )
+
+    val_dataset = FinetuneDataset(
+        data=fine_val,
+        tokenizer=tokenizer,
+        max_len=args.max_len,
+    )
+
+    test_dataset = FinetuneDataset(
+        data=fine_test,
+        tokenizer=tokenizer,
+        max_len=args.max_len,
+    )
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        shuffle=True,
+        pin_memory=True,
+    )
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        pin_memory=True,
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        pin_memory=True,
+    )
+
+    callbacks = [
+        ModelCheckpoint(
+            monitor="val_loss",
+            mode="min",
+            filename="best",
+            save_top_k=1,
+            save_last=True,
+            verbose=True,
+            dirpath=args.checkpoint_dir,
+        ),
+        LearningRateMonitor(logging_interval="step"),
+        EarlyStopping(monitor="val_loss", patience=5, verbose=True, mode="min"),
+    ]
+    wandb_logger = WandbLogger(
+        project="finetune",
+        save_dir=args.log_dir,
+    )
+    trainer = pl.Trainer(
+        accelerator="gpu",
+        devices=args.gpus,
+        strategy=DDPStrategy(find_unused_parameters=True) if args.gpus > 1 else "auto",
+        precision=16,
+        check_val_every_n_epoch=1,
+        max_epochs=args.max_epochs,
+        callbacks=callbacks,
+        logger=wandb_logger,
+        log_every_n_steps=args.log_every_n_steps,
+    )
+
+    pretrained_model = BertPretrain(
+        vocab_size=tokenizer.get_vocab_size(),
+        padding_idx=tokenizer.get_pad_token_id(),
+    )
+    pretrained_model.load_state_dict(torch.load(args.pretrained_path)["state_dict"])
+
+    model = BertFinetune(
+        pretrained_model=pretrained_model,
+    )
+
+    trainer.fit(
+        model=model,
+        train_dataloaders=train_loader,
+        val_dataloaders=val_loader,
+    )
+
+    trainer.test(
+        model=model,
+        dataloaders=test_loader,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--seed", type=int, default=42, help="Random seed for reproducibility"
+    )
+    parser.add_argument(
+        "--resume",
+        action="store_true",
+        help="Flag to resume training from a checkpoint",
+    )
+    parser.add_argument(
+        "--data_dir", type=str, default="data_files", help="Path to the data directory"
+    )
+    parser.add_argument(
+        "--train_size",
+        type=float,
+        default=0.3,
+        help="Train set size for splitting the data",
+    )
+    parser.add_argument(
+        "--test_size",
+        type=float,
+        default=0.6,
+        help="Test set size for splitting the data",
+    )
+    parser.add_argument(
+        "--max_len", type=int, default=512, help="Maximum length of the sequence"
+    )
+    parser.add_argument(
+        "--batch_size", type=int, default=32, help="Batch size for training"
+    )
+    parser.add_argument(
+        "--num_workers", type=int, default=4, help="Number of workers for training"
+    )
+    parser.add_argument(
+        "--checkpoint_dir",
+        type=str,
+        default="checkpoints/finetuning",
+        help="Path to the training checkpoint",
+    )
+    parser.add_argument(
+        "--log_dir", type=str, default="logs", help="Path to the log directory"
+    )
+    parser.add_argument(
+        "--gpus", type=int, default=1, help="Number of gpus for training"
+    )
+    parser.add_argument(
+        "--max_epochs", type=int, default=10, help="Number of epochs for training"
+    )
+    parser.add_argument(
+        "--log_every_n_steps",
+        type=int,
+        default=10,
+        help="Number of steps to log the training",
+    )
+    parser.add_argument(
+        "--pretrained_path",
+        type=str,
+        default=None,
+        required=True,
+        help="Checkpoint to the pretrained model",
+    )
+
+    args = parser.parse_args()
+    main(args)
