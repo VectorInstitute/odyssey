e7c1ed8e941f44a95891b153691d8c0223ffddfb
afallah <afallah@v.cluster.local>
2024-02-13 12:26:27 -0500

    Updated embeddings.py

--- models/big_bird_cehr/embeddings.py
+++ models/big_bird_cehr/embeddings.py
@@ -147,16 +147,86 @@ class Embeddings(nn.Module):
     ) -> torch.Tensor:
         """Applies embeddings to the input features."""
         concept_embed = self.concept_embedding(concept_ids)
-        type_embed = self.token_type_embeddings(type_ids)
+        # type_embed = self.token_type_embeddings(type_ids)
         time_embed = self.time_embedding(time_stamps)
         age_embed = self.age_embedding(ages)
-        positional_embed = self.positional_embedding(visit_orders)
+        # positional_embed = self.positional_embedding(visit_orders)
         visit_segment_embed = self.visit_embedding(visit_segments)
 
         embeddings = torch.cat((concept_embed, time_embed, age_embed), dim=-1)
         embeddings = self.tanh(self.scale_back_concat_layer(embeddings))
-        embeddings = embeddings + type_embed + positional_embed + visit_segment_embed
-        embeddings = self.LayerNorm(embeddings)
-        embeddings = self.dropout(embeddings)
+        # embeddings = embeddings + type_embed + positional_embed + visit_segment_embed
+        # embeddings = self.LayerNorm(embeddings)
+        # embeddings = self.dropout(embeddings)
 
+        return embeddings
+
+
+class BigBirdEmbeddingsForCEHR(nn.Module):
+    """Construct the embeddings from word, position and token_type embeddings."""
+
+    # Copied from transformers.models.bert.modeling_bert.BertEmbeddings.__init__
+    def __init__(self, config, cehr_embedding):
+        super().__init__()
+        self.word_embeddings = None
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size)
+        self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
+        self.visit_segment_embed = None
+
+        # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
+        # any TensorFlow checkpoint file
+        self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        # position_ids (1, len position emb) is contiguous in memory and exported when serialized
+        self.position_embedding_type = getattr(config, "position_embedding_type", "absolute")
+        self.register_buffer(
+            "position_ids", torch.arange(config.max_position_embeddings).expand((1, -1)), persistent=False
+        )
+        self.register_buffer(
+            "token_type_ids", torch.zeros(self.position_ids.size(), dtype=torch.long), persistent=False
+        )
+        # End copy
+
+        self.rescale_embeddings = config.rescale_embeddings
+        self.hidden_size = config.hidden_size
+
+    def forward(
+        self, input_ids=None, token_type_ids=None, position_ids=None, inputs_embeds=None, past_key_values_length=0
+    ):
+        if input_ids is not None:
+            input_shape = input_ids.size()
+        else:
+            input_shape = inputs_embeds.size()[:-1]
+
+        seq_length = input_shape[1]
+
+        if position_ids is None:
+            position_ids = self.position_ids[:, past_key_values_length : seq_length + past_key_values_length]
+
+        # Setting the token_type_ids to the registered buffer in constructor where it is all zeros, which usually
+        # occurs when its auto-generated, registered buffer helps users when tracing the model without passing
+        # token_type_ids, solves issue #5664
+        if token_type_ids is None:
+            if hasattr(self, "token_type_ids"):
+                buffered_token_type_ids = self.token_type_ids[:, :seq_length]
+                buffered_token_type_ids_expanded = buffered_token_type_ids.expand(input_shape[0], seq_length)
+                token_type_ids = buffered_token_type_ids_expanded
+            else:
+                token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=self.position_ids.device)
+
+        if inputs_embeds is None:
+            inputs_embeds = self.word_embeddings(input_ids)
+
+        if self.rescale_embeddings:
+            inputs_embeds = inputs_embeds * (self.hidden_size**0.5)
+
+        token_type_embeddings = self.token_type_embeddings(token_type_ids)
+
+        embeddings = inputs_embeds + token_type_embeddings
+
+        position_embeddings = self.position_embeddings(position_ids)
+        embeddings += position_embeddings
+
+        embeddings = self.dropout(embeddings)
+        embeddings = self.LayerNorm(embeddings)
         return embeddings
